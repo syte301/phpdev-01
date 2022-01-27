2d011184ded5895dbb0b88eec4ceb582b1846ec0
ww301347 <ww301347@gmail.com>
2022-01-27 22:00:50 +0600

    for git

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <body>
-        <h1><?php echo "Hello, Word!"; ?></h1>
+        <h1><?php echo "Hello, Word! gor git"; ?></h1>
         I'm <?php echo $_REQUEST['name'] ?? 'Noname'; ?>
     </body>
 </html>
